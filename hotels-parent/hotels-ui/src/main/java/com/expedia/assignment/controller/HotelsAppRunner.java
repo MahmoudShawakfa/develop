c7aa9f49fa375488472ed9cc5c615e55249878d6
MahmoudShawakfa <mahmoudshawakfa@gmail.com>
2018-03-11 21:50:54 +0200

    add excutable jar

--- hotels-parent/hotels-ui/src/main/java/com/expedia/assignment/controller/HotelsAppRunner.java
+++ hotels-parent/hotels-ui/src/main/java/com/expedia/assignment/controller/HotelsAppRunner.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelsAppRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HotelsAppRunner.class);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsAppRunner.class);
+    }
 
 }
